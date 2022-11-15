2d50654833e1a9e768891b272769d37ec93b9fe1
@charly-13 <carloscc_1997@outlook.com>
2022-11-14 22:18:16 -0600

    guardar categorias en mysql

--- Controllers/Categorias.php
+++ Controllers/Categorias.php
@@ -39,9 +39,39 @@ class Categorias extends Controllers{
 				$nombre_foto = $foto['name'];
 				$type = $foto['type'];
 				$url_temp=$foto['tmp_name'];
+				// $fecha = date('ymd');
+				// $hora = date('Hms'); 
+				$imgPortada = 'portada_categoria.png';
+				if($nombre_foto != ''){
+					$imgPortada = 'img_'.md5(date('d-m-Y H:m:s')).'.jpg';
+				}
+				if($intIdcategoria == 0){
+					//CREAR
+					$request_categoria = $this->model->insertCategoria($strCategoria,$strDescripcion,$imgPortada,$intStatus);
+					$option = 1;	
+				}else{
+					//ACTUALIZAR
+					$request_categoria = $this->model->updateCategoria($intIdcategoria,$strCategoria,$strDescripcion,$intStatus);
+					$option = 2;
+				}
+				if($request_categoria > 1){
+					if($option = 1){
+						$arrResponse = array ('status'=>true,'msg'=>'Datos guardados correctamente');
+						if($nombre_foto != ''){	uploadImage($foto,$imgPortada);	}
+					}else{
+						$arrResponse = array ('status'=>true,'msg'=>'Datos Actualizados correctamente');
+					}
+
+				}else if($request_categoria=='exist'){
+					$arrResponse = array ('status'=>false,'msg'=>'¡Atención! La categoría ya existe.');
+				} else{
+					$arrResponse = array ('status'=>false,'msg'=>'No es posible almacenar los datos.');
+				}
 
 			}
+			echo json_encode($arrResponse,JSON_UNESCAPED_UNICODE);
 		}
+		die();
 
 	}
 
